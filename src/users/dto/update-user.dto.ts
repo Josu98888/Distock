ae1f04a3bd5fc1e8d8333d23f49eca28635e36d2
Josu <aquinojosue889@gmail.com>
2026-08-12 01:41:25 -0300

    refactor: refactor update-user-dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, OmitType } from '@nestjs/mapped-types';
+import { IsBoolean, IsOptional } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(
+  OmitType(CreateUserDto, ['password'] as const),
+) {
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -59,4 +59,5 @@ export class UsersService {
   async findByEmail(email: string) {
     return this.prisma.user.findUnique({ where: { email } });
   }
+
 }
