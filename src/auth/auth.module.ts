98e6c155f36d7327ce3baea9708bad65d1651e58
Josu <aquinojosue889@gmail.com>
2026-08-14 01:41:35 -0300

    feat:add refresh-token guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,20 @@ import { UsersModule } from '@/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule, JwtSignOptions } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './guards/auth.guard';
+import { RefreshTokenGuard } from './guards/refresh-token.guard';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    RefreshTokenGuard,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   imports: [
     UsersModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
