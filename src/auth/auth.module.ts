66f9f304003eb041ec02f020cfcf4f4577232319
Josu <aquinojosue889@gmail.com>
2026-08-14 01:59:59 -0300

    refactor: modificated authmodule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,39 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule, JwtSignOptions } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersModule } from '@/users/users.module';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule, JwtSignOptions } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './guards/auth.guard';
-import { RefreshTokenGuard } from './guards/refresh-token.guard';
+import { RefreshTokenGuard } from './guards/refresh-token.guard'; // 👈 Agregamos este
 
 @Module({
-  controllers: [AuthController],
-  providers: [
-    AuthService,
-    RefreshTokenGuard,
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
-  ],
   imports: [
     UsersModule,
-    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
-        secret: config.get<string>('JWT_SECRET'),
+        secret: config.getOrThrow<string>('JWT_SECRET'),
         signOptions: {
           expiresIn: config.get<string>(
             'JWT_EXPIRES_IN',
+            '15m',
           ) as JwtSignOptions['expiresIn'],
         },
       }),
     }),
   ],
+  controllers: [AuthController],
+  providers: [AuthService, RefreshTokenGuard],
+  exports: [JwtModule],
 })
 export class AuthModule {}
