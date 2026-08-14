d5a71031d951c0da7218589bb7ecedc9f32e8fd7
Josu <aquinojosue889@gmail.com>
2026-08-14 02:06:50 -0300

    feat: add provider app_guard to app_module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
-import { APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core';
+import { APP_INTERCEPTOR, APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { AuthModule } from './auth/auth.module';
+import { AuthGuard } from './auth/guards/auth.guard';
+import { RolesGuard } from './auth/guards/roles.guard';
 
 @Module({
   imports: [
@@ -21,9 +23,19 @@ import { AuthModule } from './auth/auth.module';
   ],
   controllers: [AppController],
   providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    }, // 1º: ¿Tenés un token válido? (pone el user en el request)
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    }, // 2º: ¿Tenés el rol necesario? (lee el user del request)
+
+    // --- TUS HERRAMIENTAS PERSONALIZADAS ---
     { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
     { provide: APP_FILTER, useClass: HttpExceptionFilter },
-    AppService,
   ],
 })
 export class AppModule {}
