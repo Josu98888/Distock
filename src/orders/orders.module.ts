3f3010844bcd50b50499b3217055d994ab9f6d1b
Josu <aquinojosue889@gmail.com>
2026-08-26 00:06:59 -0300

    feat:add service orders

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '@/prisma/prisma.module';
+import { CustomersModule } from '@/customers/customers.module';
+import { OrdersService } from './orders.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CustomersModule],
+  providers: [OrdersService],
+  exports: [OrdersService],
 })
 export class OrdersModule {}
